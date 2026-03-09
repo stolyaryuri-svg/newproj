25be2ef07e94cec12de96cfb24ad0e7d595466f4
y.stolyarchuk <stolyar.yuri@gmail.com>
2026-03-09 18:28:39 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -11,31 +11,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/userdb";
-        String username = "postgres";
-        String pass = "password";
-
-        try    {
-            Connection connect = DriverManager.getConnection(url,username, pass);
-            Statement statement = connect.createStatement();
-            statement.executeUpdate("INSERT INTO customers (customer_id,first_name,age) VALUES (2, 'Jack', 23)");
-            //ResultSet result = result.executeQuery("SELECT firstname FROM customers");
-
-            //while (result.next()){
-            //    String fst = result.getString(1);
-            //    System.out.printf("%s",fst);
-            //}
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        //код для новой ветки
-        //calltest call1 = new calltest();
-        //call1.pay();
-
         System.out.printf("SQL connection");
-        //System.out.printf("SQL new line");
-        //тестирование вывода
         //в новой ветке есть новый код
     }
 }
